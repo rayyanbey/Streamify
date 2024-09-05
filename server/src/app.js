cfcc62535fc2a7c3d0db1dec48c3c0f9eb368fce
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-09-05 20:18:29 +0500

    subscription router

--- server/src/app.js
+++ server/src/app.js
@@ -24,9 +24,11 @@ app.use(cookieParser()) //Cookies Check
 //ROUTES import
 import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
 app.use('/api/v1/videos',videoRouter)
+app.use('/api/v1/subsription',subscriptionRouter)
 
 export { app }
